cda9eb245a0525e4d5c54895f3696e1f6298966a
Mark Kessler <markkessler66@gmail.com>
2022-08-02 11:16:35 -0500

    Centering, nav bar docs redesign

--- js/docs.js
+++ js/docs.js
@@ -2,12 +2,29 @@
 function openNav() {
         document.getElementById("mySidenav").style.width = "250px";
         document.getElementById("main").style.marginLeft = "250px";
-        document.getElementById("hammy").style.display = "none"
+        document.getElementById("hammy").style.display = "none";
 }
       
 /* Set the width of the side navigation to 0 and the left margin of the page content to 0 */
 function closeNav() {
         document.getElementById("mySidenav").style.width = "0";
         document.getElementById("main").style.marginLeft = "0";
-        document.getElementById("hammy").style.display = "block"
+        document.getElementById("hammy").style.display = "block";
 } 
+
+
+function activeLink(num){
+        console.log("GRRRRR");
+        for(i=1; i<=8; i++){
+                if(i - num == 0){
+                        var link = document.getElementById("link" + i.toString());
+                        link.classList.add('link-clicked');  
+                        console.log("GRRRRR2");
+                }else{
+                        var link = document.getElementById("link" + i.toString());
+                        link.classList.remove('link-clicked'); 
+                        console.log("GRRRRR3");
+                }
+        }
+        
+}
