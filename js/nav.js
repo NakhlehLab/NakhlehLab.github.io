9bcfe86f8859f7b83787540e04d4048af5ddfa76
Mark Kessler <markkessler66@gmail.com>
2022-08-08 11:09:10 -0500

    Faq page

--- js/nav.js
+++ js/nav.js
@@ -35,6 +35,27 @@ function openTab(evt, tabName) {
         evt.currentTarget.className += " active";
 } 
 
+function openFAQ(evt, question) {
+        // Declare all variables
+        var i, tabcontent, tablinks;
+      
+        // Get all elements with class="tabcontent" and hide them
+        tabcontent = document.getElementsByClassName("vertical-tabcontent");
+        for (i = 0; i < tabcontent.length; i++) {
+          tabcontent[i].style.display = "none";
+        }
+      
+        // Get all elements with class="tablinks" and remove the class "active"
+        tablinks = document.getElementsByClassName("vertical-tablinks");
+        for (i = 0; i < tablinks.length; i++) {
+          tablinks[i].className = tablinks[i].className.replace(" active", "");
+        }
+      
+        // Show the current tab, and add an "active" class to the link that opened the tab
+        document.getElementById(question).style.display = "block";
+        evt.currentTarget.className += " active";
+} 
+
 
 function SwitchPage(slideNum){
 
