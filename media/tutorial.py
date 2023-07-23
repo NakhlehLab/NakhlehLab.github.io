6dcd90e9a8fc0b4dec8f2e9b69d00b2446c449fc
markkessler66 <markkessler66@gmail.com>
2023-07-22 23:46:28 -0600

    Naming edits

--- media/tutorial.py
+++ media/tutorial.py
@@ -1,47 +1,45 @@
 import random
 from PhyNetPy.Graph import DAG
-from PhyNetPy.PhyAllop import MP_ALLOP
+from PhyNetPy.MPSugar import MP_SUGAR
 
 #Import the following two only if using subgenome map autogeneration capabilities
 from PhyNetPy.NetworkParser import NetworkParser
 from PhyNetPy.GeneTrees import GeneTrees
 
 
-
-
-### STEP 1: COMPUTE SUBGENOME MAPPINGS (MANUALLY or AUTOMATICALLY)
+### STEP 1: COMPUTE TAXON MAPPINGS (MANUALLY or AUTOMATICALLY)
 
 #Step 1a) Manual/Hardcoded mapping
 
 # TRIMMED SCENARIO J
-subgenome_map_trimmedJ : dict[str, list[str]]=  {'U': ['01uA', '01uB'], 'T': ['01tA', '01tB'], 'B': ['01bA'], 'F': ['01fA'], 
-                                                 'V': ['01vB', '01vA'], 'C': ['01cA'], 'A': ['01aA'], 'D': ['01dA'], 'O': ['01oA']} 
+# taxon_map_trimmedJ : dict[str, list[str]]=  {'U': ['01uA', '01uB'], 'T': ['01tA', '01tB'], 'B': ['01bA'], 'F': ['01fA'], 
+#                                                'V': ['01vB', '01vA'], 'C': ['01cA'], 'A': ['01aA'], 'D': ['01dA'], 'O': ['01oA']} 
 
 # UNTRIMMED SCENARIO J
-# subgenome_mapJ : dict[str, list[str]]=  {'F': ['01fA'], 'T': ['01tA', '01tB'], 'W': ['01wA', '01wB'], 'B': ['01bA'], 
-#                    'V': ['01vA', '01vB'], 'A': ['01aA'], 'U': ['01uA', '01uB'], 'C': ['01cA'], 
-#                    'E': ['01eA'], 'X': ['01xA', '01xB'], 'Y': ['01yA', '01yB'], 'O': ['01oA'], 
-#                    'Z': ['01zB', '01zA'], 'D': ['01dA']} 
+taxon_mapJ : dict[str, list[str]]=  {'F': ['01fA'], 'T': ['01tA', '01tB'], 'W': ['01wA', '01wB'], 'B': ['01bA'], 
+                   'V': ['01vA', '01vB'], 'A': ['01aA'], 'U': ['01uA', '01uB'], 'C': ['01cA'], 
+                   'E': ['01eA'], 'X': ['01xA', '01xB'], 'Y': ['01yA', '01yB'], 'O': ['01oA'], 
+                   'Z': ['01zB', '01zA'], 'D': ['01dA']} 
 
 #Step 1b) Automatically generated mapping
 
 # TRIMMED SCENARIO J
 # gene_trees_trimmedJ : list[DAG] = NetworkParser(".../Path/to/J_trimmed.nex").get_all_networks()
-# subgenome_map_trimmedJ = GeneTrees(gene_trees_trimmedJ).mp_allop_map()
+# taxon_map_trimmedJ = GeneTrees(gene_trees_trimmedJ).mp_sugar_map()
 
 # UNTRIMMED SCENARIO J
 # gene_trees_J : list[DAG] = NetworkParser(".../Path/to/J_untrimmed.nex").get_all_networks()
-# subgenome_mapJ = GeneTrees(gene_trees_J).mp_allop_map()
+# taxon_mapJ = GeneTrees(gene_trees_J).mp_sugar_map()
 
 
 #STEP 2:
-#Choose how many hill climbing chains to do. We recommend doing at least 8-10 for the untrimmed version, and 3 should sufficed for the untrimmed version.
+#Choose how many hill climbing chains to do. We recommend doing at least 5 for the untrimmed version, and 3 should suffice for the untrimmed version.
 num_hill_climbing_chains = 3
 
 for dummy in range(num_hill_climbing_chains):
     
     #change pathname to unpruned nexus file if doing that version.
-    output_networks : dict[tuple[DAG, int]] = MP_ALLOP(".../Path/to/J_trimmed.nex", subgenome_map_trimmedJ, iter_ct = 500, seed = random.randint(0, 1000))
+    output_networks : dict[tuple[DAG, int]] = MP_SUGAR(".../Path/to/J_trimmed.nex", taxon_mapJ, iter_ct = 500, seed = random.randint(0, 1000))
 
     #STEP 3: Analyze output
     for net, score in output_networks.items():
